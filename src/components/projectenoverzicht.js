420e49b59a930d17101aa7baec967820d82ffa41
Mguy11 <0911554@hr.nl>
2019-05-06 00:08:40 +0200

    Quickfix voor de routing van de projecten.

--- src/components/projectenoverzicht.js
+++ src/components/projectenoverzicht.js
@@ -16,49 +16,49 @@ class Projectenoverzicht extends Component {
                 </Cell>
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                    <CardActionArea href="/reactportfolio">
+                    <CardActionArea href="./reactportfolio">
                         <Card.Img src="/images/Portfolio_React_Thumbnail.png" alt="Card image" />
                         </CardActionArea>
                     </Card>
                 </Cell>
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/dinorun">
+                        <CardActionArea href="./dinorun">
                             <Card.Img src="/images/Dinorun_Thumbnail.png" alt="Card image" />
                         </CardActionArea>
                     </Card>
                 </Cell>   
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/karinsfashion">
+                        <CardActionArea href="./karinsfashion">
                             <Card.Img src="/images/Karins_Fashion_Thumbnail.png" alt="Card image" />
                         </CardActionArea>
                     </Card>
                 </Cell>
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/ricadolips">
+                        <CardActionArea href="./ricadolips">
                             <Card.Img src="/images/Ricardo_Lips_Thumbnail.png" alt="Card image"/>
                         </CardActionArea>
                     </Card>
                 </Cell>
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/motument">
+                        <CardActionArea href="./motument">
                             <Card.Img src="/images/Motument_Voor_Jou_Thumbnail.png" alt="Card image" />
                         </CardActionArea>       
                     </Card>
                 </Cell>    
                 <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/vueapp">
+                        <CardActionArea href="./vueapp">
                             <Card.Img src="/images/Vue_App_Thumbnail_1.png" alt="Card image" />
                         </CardActionArea>
                     </Card>
                 </Cell>
                 <Cell col={6} className="zoom"> 
                     <Card className="bg-dark text-white">
-                        <CardActionArea href="/rooisklumpke">
+                        <CardActionArea href="./rooisklumpke">
                             <Card.Img src="/images/St_Oedenrode_Thumbnail.png" alt="Card image" />
                         </CardActionArea>
                     </Card>
