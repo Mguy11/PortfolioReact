9e8c5d86a532ba5dfe64a588f66762ddccc3ea90
Mguy11 <0911554@hr.nl>
2019-05-06 19:51:24 +0200

    1e versie weer terug gezet.

--- src/components/projectenoverzicht.js
+++ src/components/projectenoverzicht.js
@@ -1,86 +1,66 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
 
 class Projectenoverzicht extends Component {
     
       render() {
         return (
-            
             <Grid className="projects-grid">  
                 <Cell col={12}>
                     <div className="projects-titel">
                         <h1>Mijn Projecten</h1>
                     </div>
                 </Cell>
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
-                            <Card.Img src="/images/Portfolio_React_Thumbnail.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/reactportfolio" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>
+                    <CardActionArea href="/reactportfolio">
+                        <Card.Img src="/images/Portfolio_React_Thumbnail.png" alt="Card image" />
+                        </CardActionArea>
                     </Card>
                 </Cell>
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/dinorun">
                             <Card.Img src="/images/Dinorun_Thumbnail.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/dinorun" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>
+                        </CardActionArea>
                     </Card>
                 </Cell>   
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/karinsfashion">
                             <Card.Img src="/images/Karins_Fashion_Thumbnail.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/karinsfashion.js" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>
+                        </CardActionArea>
                     </Card>
                 </Cell>
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/ricadolips">
                             <Card.Img src="/images/Ricardo_Lips_Thumbnail.png" alt="Card image"/>
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/ricadolips" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>
+                        </CardActionArea>
                     </Card>
                 </Cell>
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/motument">
                             <Card.Img src="/images/Motument_Voor_Jou_Thumbnail.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/motument" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>    
+                        </CardActionArea>       
                     </Card>
                 </Cell>    
-                <Cell col={6}>
+                <Cell col={6} className="zoom">
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/vueapp">
                             <Card.Img src="/images/Vue_App_Thumbnail_1.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/vueapp" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>  
+                        </CardActionArea>
                     </Card>
                 </Cell>
-                <Cell col={6}> 
+                <Cell col={6} className="zoom"> 
                     <Card className="bg-dark text-white">
+                        <CardActionArea href="/rooisklumpke">
                             <Card.Img src="/images/St_Oedenrode_Thumbnail.png" alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Body>
-                                    <Button variant="secondary" href="/rooisklumpke" className="project-button">Go somewhere</Button>
-                                </Card.Body>
-                            </Card.ImgOverlay>
+                        </CardActionArea>
                     </Card>
                 </Cell>
           </Grid>
