9c84c4ddfbfb91613f8fd033c12f880ba28e4fcd
Mguy11 <0911554@hr.nl>
2019-05-18 22:17:49 +0200

    Website gefixt met content verbetering + landingpage update.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom'
@@ -14,7 +13,4 @@ ReactDOM.render(
    
     , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
